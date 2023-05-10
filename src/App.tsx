332161e4eb71180605222cc3a32877aecc6407ad
valerite-dev <valerite.work@gmail.com>
2023-05-10 17:24:47 +0300

    MB-3: added mui/joy

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
-import LocalViewStore from './stores/LocalStore';
-
-function Modal() {
-  return <div>The Modal</div>;
-}
-
-const Page = observer(() => {
-  const [modalStore] = useState(() => new LocalViewStore('ModalViewStore'));
-
-  useEffect(
-    // stop writing to the store when component unmounted
-    () => () => modalStore.stopStore(),
-    [modalStore],
-  );
-
-  return (
-    <main>
-      <h1>Page title</h1>
-      <button onClick={modalStore.toggleModal} type="button">Toggle Modal</button>
-      {modalStore.modal.visibility ? <Modal /> : null}
-    </main>
-  );
-});
+import HomePage from './pages/home/HomePage';
 
 function App() {
-  return <Page />;
+  return <HomePage />;
 }
 
 export default App;
